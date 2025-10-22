20118a95256dd1fffdefa03370db6ba42c43613e
shad0w <kirillpashnin.dev@gmail.com>
2025-10-22 12:12:20 +0200

    Added rooms feature

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,31 @@
 <script setup>
-import { ref } from "vue";
+import { ref, provide } from "vue";
 import Canvas from "./components/Canvas.vue";
+import Rooms from "./components/Rooms.vue";
+import SocketManager from "../services/socketManager.js";
+
+const manager = new SocketManager();
+manager.connect();
+//So i can use it in every other component
+provide("socketManager", manager);
+
+const inRoom = ref(false);
+const room = ref([]);
 </script>
 
 <template>
-  <h1>Co-working canvas</h1>
-  <Canvas />
+  <Canvas v-if="inRoom" :room="room" />
+  <Rooms
+    v-else
+    @isInRoom="(msg) => (inRoom = msg)"
+    @roomName="(roomData) => (room = roomData)"
+  />
 </template>
 
-<style scoped></style>
+<style>
+body {
+  color: white;
+  font-family: sans-serif;
+  background-color: #000919;
+}
+</style>
